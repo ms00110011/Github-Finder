201d954710392fa8c5e0c395275883b9b8d7ea85
ms00110011 <ms00110011@gmail.com>
2022-06-02 12:37:39 +0530

    Profile page completed

---
+++ src/Components/Profile Page/ProfileCard.jsx
@@ -0,0 +1,56 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import styles from "./ProfileCard.module.css";
+
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function ProfileCard({img,name}) {
+  const [expanded, setExpanded] = React.useState(false);
+
+
+
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const [bio,setbio] = React.useState("")
+
+
+  return (
+    <Card sx={{ maxWidth: 250, maxHeight:250 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: red[500]}} aria-label="recipe">
+            <img style={{width:"100%"}} src={img} alt="" />
+          </Avatar>
+        }
+        title = {name}
+      />
+    </Card>
+  );
+}
